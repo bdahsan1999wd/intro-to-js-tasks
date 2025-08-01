580ca74427119d371dba0ccb99c2ed7f820f388b
bdahsan1999wd <bdahsan1999wd@gmail.com>
2025-08-01 23:23:43 +0600

    task-2 added

---
+++ task-2.js
@@ -0,0 +1,18 @@
+var mathematics = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bengal = 99.50;
+console.log(mathematics, biology, chemistry, physics, bengal);
+
+var total = mathematics + biology + chemistry + physics + bengal;
+// console.log(total);
+
+var average = total / 5;
+// console.log(average);
+
+var averageNumber = average.toFixed(2);
+// console.log(averageNumber);
+
+average = parseFloat(averageNumber);
+console.log(average);
